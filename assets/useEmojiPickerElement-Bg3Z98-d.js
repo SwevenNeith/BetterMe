2e60a31187a9a07dac727c530fe327fb7f7e4318
CamilleFourRF <cfour@grouperf.com>
2026-06-29 15:14:21 +0200

    Updates

--- assets/useEmojiPickerElement-Bg3Z98-d.js
+++
@@ -1,1 +0,0 @@
-import{pt as e}from"./index-CmtHhroq.js";var t=`https://cdn.jsdelivr.net/npm/emoji-picker-element-data@^1/fr/emojibase/data.json`,n=null,r=null;function i(){return r||=Promise.all([e(()=>import(`./emoji-picker-element-CNuAAKyC.js`),[]),e(()=>import(`./fr-CjVNt1oi.js`),[])]).then(([,e])=>{n=e.default}),r}function a(e){!e||!n||(e.i18n=n)}export{a as n,i as r,t};
